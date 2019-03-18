1a0968540be121320d760596a67150552207201a
Myles Bennett <myles.bennett@hotmail.com>
2019-03-18 14:02:33 -0400

    added date validators for project create form

--- crowdfunder/forms.py
+++ crowdfunder/forms.py
@@ -4,9 +4,11 @@ from django.forms import ModelForm, Form, CharField, DateField, PasswordInput, E
 from crowdfunder.models import Profile, Project, Reward, Donation
 from django import forms
 
+
 class LoginForm(Form):
     username = CharField(label="User Name", max_length=64)
     password = CharField(widget=PasswordInput())
+
 
 class ProfileForm(ModelForm):
     first_name = CharField()
@@ -17,9 +19,22 @@ class ProfileForm(ModelForm):
         model = Profile
         fields = ['first_name', 'last_name', 'email']
 
+
 class ProjectForm(ModelForm):
     funding_start_date = DateField(widget=DateInput(attrs={'type': 'date', 'min': dt.date.today() }))
     funding_end_date = DateField(widget=DateInput(attrs={'type': 'date', 'min': dt.date.today() }))
+
     class Meta:
         model = Project
         fields = ['name', 'description', 'funding_start_date', 'funding_end_date', 'goal']
+
+    def clean_start_date(self):
+        cleaned_date = self.cleaned_data['funding_start_date']
+        if cleaned_date < dt.date.today():
+            self.add_error('Start date must be in the future.')
+
+    def clean_end_date(self):
+        cleaned_start_date = self.cleaned_data['funding_start_date']
+        cleaned_end_date = self.cleaned_data['funding_end_date']
+        if cleaned_start_date > cleaned_end_date:
+            self.add_error('End date must be after start date.')
