a21be6041c37e50a69e5951e04107469029f297d
Anton <anton.mois33v@gmail.com>
2019-03-18 12:01:34 -0400

    Implemented the projects page and create form

---
+++ crowdfunder/forms.py
@@ -0,0 +1,24 @@
+import datetime as dt
+from django.core.exceptions import ValidationError
+from django.forms import ModelForm, Form, CharField, DateField, PasswordInput, EmailField, DateInput
+from crowdfunder.models import Profile, Project, Reward, Donation
+
+class LoginForm(Form):
+    username = CharField(label="User Name", max_length=64)
+    password = CharField(widget=PasswordInput())
+
+class ProfileForm(ModelForm):
+    first_name = CharField()
+    last_name = CharField()
+    email = EmailField()
+
+    class Meta:
+        model = Profile
+        fields = ['first_name', 'last_name', 'email']
+
+class ProjectForm(ModelForm):
+    funding_start_date = DateField(widget=DateInput(attrs={'type': 'date', 'min': dt.date.today() }))
+    funding_end_date = DateField(widget=DateInput(attrs={'type': 'date', 'min': dt.date.today() }))
+    class Meta:
+        model = Project
+        fields = ['name', 'description', 'funding_start_date', 'funding_end_date', 'goal']
--- crowdfunder/views.py
+++ crowdfunder/views.py
@@ -1,4 +1,5 @@
 from crowdfunder.models import Profile, Project, Reward, Donation
+from crowdfunder.forms import ProjectForm
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 
@@ -29,13 +30,22 @@ def signup(request):
     return HttpResponse(html_response)
 
 def project_page(request):
-    pass
+    context = {'projects': Project.objects.all()}
+    return render(request, 'projects.html', context)
 
 def show_project(request, project_id):
     pass
 
 def create_project(request):
-    pass
+    if request.method == "POST":
+        form = ProjectForm(request.POST)
+        project = form.instance
+        project.owner = request.user
+        if form.is_valid():
+            form.save()
+    else:
+        form = ProjectForm()
+    return render(request, 'create_project.html', {'form': form})
 
 def profile(request):
     pass
